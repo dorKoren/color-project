5a15c063edd7a1261ab582f3a81257eecc64e5f8
dor.koren <dor.koren@post.idc.ac.il>
2020-07-26 16:24:15 +0300

    Creating the Palette Componen

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div>
+                <div className='Palette'>
+                    <div className='Paletter-colors'></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Palette;
