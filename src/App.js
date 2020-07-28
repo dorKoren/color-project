5c26d74fc400b43f4d75c0895a4c065502048572
dor.koren <dor.koren@post.idc.ac.il>
2020-07-28 16:23:52 +0300

    Finding Palettes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(palette => {return palette.id === id});
+  }
+
   render() {
     return (
       <Switch>
-        <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-        <Route exact path='/palette/:id' render={() => <h1>TEST</h1>} />
+
+        <Route 
+          exact 
+          path='/' 
+          render={
+            () => <h1>PALETTE LIST GOES HERE</h1>
+          } 
+        />
+
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={
+            routeProps => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+          } 
+        />
+
       </Switch>
 
       // <div className="App">
