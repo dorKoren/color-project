c8901f8fe1487e94fc399f6285dc920402aed7da
dor.koren <dor.koren@post.idc.ac.il>
2020-07-29 10:54:46 +0300

    Finishing Up MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -16,7 +16,11 @@ const styles = {
     },
 
     colors: {
-        backgroundColor: 'gray'
+        backgroundColor: '#dae1e4',
+        height: '150px',
+        wisth: '100%',
+        borderRadius: '5px',
+        overflow: 'hidden'
     },
 
     title: {
@@ -32,15 +36,33 @@ const styles = {
     emoji: {
         marginLeft: '.5rem',
         fontSize: '1.5rem'
+    },
+
+    miniColor: {
+        height: '25%',
+        width: '20%',
+        display: 'inline-block',
+        margin: '0 auto',
+        position: 'relative',
+        marginBottom: '-3.5px'
     }
 };
 
 function MiniPalette(props) {
-    const { classes, paletteName, emoji } = props;
+
+    const { classes, paletteName, emoji, colors } = props;
+
+    const miniColorBoxes = colors.map(color => (
+        <div 
+            className={classes.miniColor} 
+            style={{backgroundColor: color.color}}
+            key={color.name}
+        />
+    ));
 
     return (
         <div className={classes.root}>
-            <div className={classes.colors} />
+            <div className={classes.colors}>{miniColorBoxes}</div>
             <h5 className={classes.title}>{paletteName} 
                 <span className={classes.emoji}>{emoji}</span>
             </h5>
