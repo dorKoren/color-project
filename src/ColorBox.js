6c1c8d733c79ad858b165b4c413d0ace83f39b40
dor.koren <dor.koren@post.idc.ac.il>
2020-08-01 12:10:45 +0300

    Finish Refactoring Color Box

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -19,14 +19,17 @@ const styles = {
       opacity: 1
     }
   },
+
   copyText: {
     color: props =>
       chroma(props.background).luminance() >= 0.7 ? "black" : "white"
   },
+
   colorName: {
     color: props =>
       chroma(props.background).luminance() <= 0.08 ? "white" : "black"
   },
+
   seeMore: {
     color: props =>
       chroma(props.background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
@@ -41,6 +44,7 @@ const styles = {
     lineHeight: "30px",
     textTransform: "uppercase"
   },
+
   copyButton: {
     color: props =>
       chroma(props.background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
@@ -61,6 +65,73 @@ const styles = {
     border: "none",
     textDecoration: "none",
     opacity: 0
+  },
+
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px'
+  },
+
+  copyOverlay: {
+    zIndex: '0',
+    opacity: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)'
+  },
+
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute'
+  },
+
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    "& h1": {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase'
+    },
+
+    "& p": {
+      fontSize: '2rem',
+      fontWeight: '100'
+    }
+  },
+
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.3s'
   }
 };
 
@@ -89,14 +160,14 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && "show"}`}
+            className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
           />
-          <div className={`copy-msg ${copied && "show"}`}>
+          <div className={`${classes.copyMessage} ${copied && clasees.showMessage}`}>
             <h1>copied!</h1>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
-          <div className='copy-container'>
-            <div className='box-content'>
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
