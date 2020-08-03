e254d2201397846215a362f59f5bac8de2b5604b
dor.koren <dor.koren@post.idc.ac.il>
2020-08-03 15:16:38 +0300

    Creating Draggable Color Box

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,8 +11,8 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
+import DraggableColorBox from './DraggableColorBox';
 import { ChromePicker } from "react-color"; 
-import { colors } from "@material-ui/core";
 
 const drawerWidth = 400;
 
@@ -20,12 +20,14 @@ const styles = theme => ({
   root: {
     display: "flex"
   },
+
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     })
   },
+
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
@@ -34,20 +36,25 @@ const styles = theme => ({
       duration: theme.transitions.duration.enteringScreen
     })
   },
+
   menuButton: {
     marginLeft: 12,
     marginRight: 20
   },
+
   hide: {
     display: "none"
   },
+
   drawer: {
     width: drawerWidth,
     flexShrink: 0
   },
+
   drawerPaper: {
     width: drawerWidth
   },
+
   drawerHeader: {
     display: "flex",
     alignItems: "center",
@@ -55,8 +62,10 @@ const styles = theme => ({
     ...theme.mixins.toolbar,
     justifyContent: "flex-end"
   },
+
   content: {
     flexGrow: 1,
+    height: "calc(100vh - 64px)",
     padding: theme.spacing(3),
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
@@ -64,11 +73,13 @@ const styles = theme => ({
     }),
     marginLeft: -drawerWidth
   },
+
   contentShift: {
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen
     }),
+
     marginLeft: 0
   }
 });
@@ -172,11 +183,11 @@ class NewPaletteForm extends Component {
             })}
             >
             <div className={classes.drawerHeader} />
-            <ul>
-                {colors.map(color => (
-                    <li style={{backgroundColor: color}}>{color}</li>
-                ))}
-            </ul>
+
+            {this.state.colors.map(color => (
+                <DraggableColorBox color={color} />
+            ))}
+
             </main>
         </div>
         );
